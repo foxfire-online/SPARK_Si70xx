c1a6d795ecbbc71644295ad93b50fa0106675a5c
Marcus Uy <marcus.uy@hyperbit.net>
2015-07-09 18:34:37 +1000

    Bugfix
    Bugfix

--- firmware/FOXFIRE_Si70xx.cpp
+++ firmware/FOXFIRE_Si70xx.cpp
@@ -8,7 +8,7 @@ FOXFIRE_Si70xx::FOXFIRE_Si70xx()
 boolean FOXFIRE_Si70xx::begin(void)
 {
 	Wire.begin();
-	Wire.beginTransmission(addrI2C);
+	Wire.beginTransmission(_addr);
 	return ( Wire.endTransmission(true) == 0 ? true : false );
 }
 
